5e6993f962721281ab88080480ee988237470a58
morunner <107193613+morunner@users.noreply.github.com>
2025-12-16 09:45:44 +0100

    use histogram for hls/keras comparison

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -2,8 +2,10 @@ from typing import Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+import tensorflow as tf
 from keras import Model
-from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import roc_auc_score, roc_curve
 
 
 def garnet_predict(model: Model, x_test: list) -> Tuple[list, list]:
@@ -13,56 +15,69 @@ def garnet_predict(model: Model, x_test: list) -> Tuple[list, list]:
     return y_energy_pred, y_pid_pred
 
 
-def split_energy_into_electrons_pions(truth_pid: np.ndarray, y_energy: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    filter = np.array(list(map(bool, truth_pid)))
-    y_energy_e = y_energy[~filter]
-    y_energy_p = y_energy[filter]
-    return y_energy_e, y_energy_p
+def response_rmse(y_true, y_pred):
+    y_true = tf.reshape(y_true, (-1,))
+    y_pred = tf.reshape(y_pred, (-1,))
+    response = tf.math.divide_no_nan(y_pred, y_true)
+    return tf.sqrt(tf.reduce_mean(tf.square(response - 1.0)))
 
 
-def bin_ref_comp_response_by_ref_pred_energies(
-    y_energy_ref: np.ndarray, y_energy_comp: np.ndarray
-) -> Tuple[np.ndarray, np.ndarray]:
-    bins = np.linspace(10, 90, 9).astype(int)  # Create 10 GeV bins from 10 to 100 GeV
-    indices = np.digitize(y_energy_ref, bins)
-    responses = y_energy_comp / y_energy_ref
-
-    output = []
-    for i in range(1, len(bins) + 1):
-        output.append(responses[indices == i])
-
-    return output, bins
-
-
-def response_boxplot(
-    y_pid_test: np.ndarray,
-    y_energy_ref: np.ndarray,
-    y_energy_comp: np.ndarray,
-    title: str,
-    ylabel: str,
+def compare_keras_hls_predictions(
+    test_energy_pred: np.ndarray,
+    test_pid_pred: np.ndarray,
+    test_energy_pred_hls: np.ndarray,
+    test_pid_pred_hls: np.ndarray,
+    test_energy_true: np.ndarray,
+    test_pid_true: np.ndarray,
 ):
-    y_energy_ref_e, y_energy_ref_p = split_energy_into_electrons_pions(y_pid_test, y_energy_ref)
-    y_energy_comp_e, y_energy_comp_p = split_energy_into_electrons_pions(y_pid_test, y_energy_comp)
+    test_response_rmse = response_rmse(test_energy_true, test_energy_pred)
+    test_auc = roc_auc_score(test_pid_true, test_pid_pred)
+    fpr, tpr, _ = roc_curve(test_pid_true, test_pid_pred)
 
-    y_energy_e_bins, _ = bin_ref_comp_response_by_ref_pred_energies(y_energy_ref_e, y_energy_comp_e)
-    y_energy_p_bins, bins = bin_ref_comp_response_by_ref_pred_energies(y_energy_ref_p, y_energy_comp_p)
+    hls_response_rmse = response_rmse(test_energy_true, test_energy_pred_hls)
+    hls_auc = roc_auc_score(test_pid_true, test_pid_pred_hls)
+    hls_fpr, hls_tpr, _ = roc_curve(test_pid_true, test_pid_pred_hls)
 
-    rms_tot = root_mean_squared_error(y_energy_comp / y_energy_ref, np.ones(y_energy_ref.shape))
-    rms_e = root_mean_squared_error(y_energy_comp_e / y_energy_ref_e, np.ones(y_energy_ref_e.shape))
-    rms_p = root_mean_squared_error(y_energy_comp_p / y_energy_ref_p, np.ones(y_energy_ref_p.shape))
+    with sns.axes_style('darkgrid'):
+        plt.figure(figsize=(8, 4))
 
-    fig, axs = plt.subplots(1, 2, figsize=(8, 3), sharex=True, sharey=True)
-    axs[0].boxplot(y_energy_e_bins, tick_labels=bins, whis=[5, 95], showfliers=False)
-    axs[0].set_title(f'Electrons (RMS = {rms_e:.3f})')
-    axs[1].boxplot(y_energy_p_bins, tick_labels=bins, whis=[5, 95], showfliers=False)
-    axs[1].set_title(f'Pions (RMS = {rms_p:.3f})')
+        plt.subplot(1, 2, 1)
+        plt.plot(1 - fpr, tpr, label=f'Keras (AUC: {test_auc:.3f})')
+        plt.plot(1 - hls_fpr, hls_tpr, label=f'HLS (AUC: {hls_auc:.3f})')
 
-    fig.supxlabel('Primary Particle Energy (GeV)')
-    fig.supylabel(ylabel)
-    for ax in axs:
-        ax.yaxis.grid(True)
-        ax.axhline(y=1, alpha=0.4, linestyle='dotted', color='red')
+        plt.xlabel('Pion False Positive Rate')
+        plt.ylabel('Pion True Positive Rate')
+        plt.xlim(0.7, 1.0)
+        plt.ylim(0.7, 1.0)
+        plt.title('Pion Identification ROC Curve')
+        plt.legend(loc='lower right')
 
-    fig.suptitle(f'{title} (RMS = {rms_tot:.3f})')
-    fig.tight_layout()
-    fig.show()
+        plt.subplot(1, 2, 2)
+
+        plt.hist(
+            test_energy_pred.flatten() / test_energy_true,
+            bins=50,
+            histtype='stepfilled',
+            alpha=0.5,
+            density=True,
+            label=f'Keras (RMSE: {test_response_rmse:.3f})',
+        )
+
+        plt.hist(
+            test_energy_pred_hls.flatten() / test_energy_true,
+            bins=50,
+            histtype='stepfilled',
+            alpha=0.5,
+            density=True,
+            label=f'HLS (RMSE: {hls_response_rmse:.3f})',
+        )
+
+        plt.axvline(1.0, color='k', linestyle='--', lw=1, alpha=0.7)
+        plt.xlabel('Predicted / True Energy')
+        plt.ylabel('Density')
+        plt.xlim(0.0, 2.0)
+        plt.title('Energy Response Distribution')
+        plt.legend(loc='upper right')
+
+        plt.tight_layout()
+        plt.show()
